6f38deadb49fce628c6307fd2fc30b0bc15061e1
sanket <sanketgargate06@gmail.com>
2020-10-15 22:58:33 +0530

    tests

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,9 +1,44 @@
 import 'package:flutter_app/src/article.dart';
+import 'dart:convert' as  json;
 
-List<int> parseTopStories(String json) {
-  throw UnimplementedError();
+class Article {
+  final String by;
+  final int id;
+  final int score;
+  final int time;
+  final String title;
+  final String url;
+
+  const Article(
+      { this.by,
+        this.id,
+        this.title,
+        this.score,
+        this.time,
+        this.url,
+      });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    if (json == null)
+      return null;
+
+    return Article(
+        title: json['title'] ?? '[null]',
+        url: json['url'],
+        by: json['by'],
+        time: json['time'],
+        score: json['score']);
+  }
 }
 
-Article parseArticle(String json) {
-  throw UnimplementedError();
+List<int> parseTopStories(String jsonStr) {
+  final parsed = json.jsonDecode(jsonStr);
+  final listOfIds = List<int>.from(parsed);
+  return listOfIds;
+}
+
+Article parseArticle(String jsonStr) {
+  final parsed = json.jsonDecode(jsonStr);
+  Article article = Article.fromJson(parsed);
+  return article;
 }
