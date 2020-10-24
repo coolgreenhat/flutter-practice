3ccc3eac8d777108267eee8c0d2d73a8b8bca1ff
sanket <sanketgargate06@gmail.com>
2020-10-24 23:36:53 +0530

    added animation

--- lib/main.dart
+++ lib/main.dart
@@ -109,18 +109,44 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class LoadingInfo extends StatelessWidget {
+class LoadingInfo extends StatefulWidget {
   Stream<bool> _isLoading;
+
   LoadingInfo(this._isLoading);
 
+  createState() => LoadingInfoState();
+}
+
+class LoadingInfoState extends State<LoadingInfo> with TickerProviderStateMixin {
+
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this,
+    duration: Duration(seconds: 1));
+  }
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: _isLoading,
-      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-        // if (snapshot.hasData && snapshot.data)
-          return Icon(FontAwesomeIcons.hackerNewsSquare);
-        // return Container();
-      });
+        stream: widget._isLoading,
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          // if (snapshot.hasData && snapshot.data) {
+            _controller.forward().then((f) {
+              _controller.reverse();
+            });
+            return FadeTransition(
+              child: Icon(FontAwesomeIcons.hackerNewsSquare),
+              opacity: Tween(begin:0.5, end:1.0).animate(CurvedAnimation(
+                curve: Curves.easeIn, parent: _controller)
+              ), 
+            );
+          }
+          // _controller.reverse();
+          // return Container();
+        // }
+        );
+    }
   }
-}
+
