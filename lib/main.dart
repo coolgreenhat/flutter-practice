df97c05e3febac8a27398c719f718edc77d7900f
sanket <sanketgargate06@gmail.com>
2020-11-25 22:37:56 +0530

    override actions buildLeading & buildActions

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,11 @@ class _MyHomePageState extends State<MyHomePage> {
         leading: LoadingInfo(widget.bloc.isLoading),
         elevation: 0.0,
         actions: [
-         IconButton(icon: Icon(Icons.search), onPressed: () {})
+         IconButton(icon: Icon(Icons.search), onPressed: () {
+           showSearch(
+               context: context,
+               delegate: ArticleSearch());
+         })
         ]
       ),
       body: StreamBuilder<UnmodifiableListView<Article>>(
@@ -124,26 +128,30 @@ class _MyHomePageState extends State<MyHomePage> {
 class ArticleSearch extends SearchDelegate<Article> {
   @override
   List<Widget> buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    return [
+      IconButton(icon: Icon(Icons.clear), onPressed: () {
+        query = '';
+      }),
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
+    return IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          close(context,null);
+        });
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
+    return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return Container();
   }
   
 }
